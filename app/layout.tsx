d1b2893b69257ce9063acfceb883e447fe000bcf
Vinayak Ravi <vinayakravi@gmail.com>
2024-12-27 18:26:32 -0500

    removed duplicate import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 import { Metadata } from 'next';
-import { Metadata } from 'next';
  
 export const metadata: Metadata = {
   title: {
